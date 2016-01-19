505ccef809848ce71730fde24213b9880a16ea26
Joyann <joyann.init@gmail.com>
2016-01-19 23:39:06 +0800

    增加自动填充微博账号密码功能.

--- JYWeibo/JYWeibo/Classes/Visitor/Controller/LoginViewController.swift
+++ JYWeibo/JYWeibo/Classes/Visitor/Controller/LoginViewController.swift
@@ -19,15 +19,20 @@ class LoginViewController: UIViewController {
         
         // 加载授权页面
         let request = NSURLRequest(URL: NSURL(string: WEIBO_AUTH_URL)!)
+
         webView.loadRequest(request)
     }
     
     // MARK: - Private Methods
     
     @IBAction func dismiss(sender: AnyObject) {
-        
         dismissViewControllerAnimated(true, completion: nil)
-        
     }
+    
+    @IBAction func fill(sender: AnyObject) {
+        let js = "var user = '1111'; var user_input = document.getElementById('userId'); user_input.value = user; var pwd = '2222'; var pwd_input = document.getElementById('passwd'); pwd_input.value = pwd;"
+        webView.stringByEvaluatingJavaScriptFromString(js);
+    }
+    
    
 }
